e31989cb912024e0b0cc387486217fbd85e5a958
WEB <jonatar75@gmail.com>
2024-11-06 17:40:34 -0300

    up dos templates

--- fabrica/urls.py
+++ fabrica/urls.py
@@ -7,8 +7,6 @@ from fabrica import settings, views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', default, name='index'),
-
-    
     path('<str:folder_name>/<str:template_name>/', ViewRequests.as_view(), name='view_requests'),
     path('cadastrar/', views.cadastrar_aluno, name='cadastrar_aluno'),
     path('alunos/', views.lista_alunos, name='lista_alunos'),
